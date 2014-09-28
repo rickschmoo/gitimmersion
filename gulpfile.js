730a276402e9eed85d239987cfaa86f2c314e791
Richard Boardman <rickschmoo@gmail.com>
2014-09-28 09:17:13 -0700

    added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+gulp.task('sass', function() {
+  return gulp.src('src/styles/main.scss')
+    .pipe(sass({ style: 'compressed' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('dist/assets/css'))
+});
